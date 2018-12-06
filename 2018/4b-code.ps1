7a8f0ff7b9ba53636b7580dbf0acb9b460deac10
Mark Domansky <4317290+markdomansky@users.noreply.github.com>
2018-12-05 20:49:03 -0600

    Added 2018/4

---
+++ 2018/4b-code.ps1
@@ -0,0 +1,68 @@
+$inputs = gc .\4-input.txt | sort
+$imax = $inputs.Count
+$records = New-Object System.Collections.Generic.List[pscustomobject]
+
+$GuardID = $null
+$newobj = $null
+for ($i=0;$i -lt $imax; $i++) {
+    $row = $inputs[$i]
+    $rx1 = "\[(?<dt>\d{4}-\d{2}-\d{2} \d{2}:\d{2})\] Guard #(?<IDX>\d+) begins shift"
+    $rx2 = "\[(?<dt>\d{4}-\d{2}-\d{2} \d{2}:\d{2})\] falls asleep"
+    $rx3 = "\[(?<dt>\d{4}-\d{2}-\d{2} \d{2}:\d{2})\] wakes up"
+
+    if ($row -match $rx1) {
+        $guardid = $matches["IDX"]
+    } elseif ($row -match $rx2) {
+        $newobj = [PSCustomObject]@{
+            GuardID = $GuardID
+            DTStart = get-date $matches["dt"]
+            DTEnd = $null
+            Delta = 0
+        }
+    } elseif ($row -match $rx3) {
+        $newobj.DTEnd = get-date $matches["dt"]
+        $newobj.delta = ($newobj.dtend - $newobj.dtstart).totalminutes
+        $records.add($newobj)
+        $newobj = $null
+    } else {
+        write-host "No RX match for '$row'"
+    }
+
+}
+
+#calc which minutes each guard is asleep
+$guards = $records | group GuardID 
+
+#calculate most asleep minute per guard
+$guards | %{$_ | add-member -name "Sleep" -MemberType NoteProperty -value 0}
+$guards | %{$_.sleep = (($_.group | measure delta -sum).sum)}
+
+
+
+$guards | %{$_ | add-member -MemberType noteproperty -Name SleepSched -Value $null}
+
+$guards | %{$_.sleepsched = (New-Object System.Collections.Generic.List[pscustomobject])}
+foreach ($guard in $guards) {
+    for ($min=0;$min -lt 60; $min++) {
+        $rec = [pscustomobject]@{"Min"=$min;"SleepCount"=0}
+        foreach ($row in $guard.group) {
+            #"$min $($row.dtstart) $($row.dtend)"
+            if ($row.dtstart.minute -le $min -and $row.dtend.minute -gt $min) {$rec.sleepcount++;}
+        }
+        $guard.sleepsched.add($rec)
+    }
+}
+
+$guards | %{$_ | add-member -MemberType noteproperty -Name SleepMin -Value $null}
+$guards | %{$_ | add-member -MemberType noteproperty -Name SleepMinCnt -Value $null}
+
+$guards | %{$_.sleepmin = 0}
+$guards | %{$_.sleepminCnt = 0}
+foreach ($guard in $guards) {
+    $max = ($guard.sleepsched | sort sleepcount -desc | select -first 1)
+    $guard.sleepmin = $max.Min
+    $guard.sleepmincnt = $max.sleepcount
+}
+
+$guards | sort sleepmincnt -desc | select -first 1 | ft name,sleep,sleepmin,@{Label="Mult";Expression={[int64]$_.name * $_.SleepMin} }
+
